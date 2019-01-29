b09e13c1dbefd5c675954b03b6565d7d20f8c746
Min Zeng <zengmin@csu.edu.cn>
2019-01-29 20:06:58 +0800

    Add files via upload

---
+++ hyperparams.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# /usr/bin/python2
+
+
+class Hyperparams:
+    '''Hyperparameters'''
+    # data
+    static_random_seed = 3
+    tf_random_seed = 3
+    neg_pos_ratio = 1
+    train_val_ratio = 0.9
+    batch_size = 32
+    epoch_num = 30  # 200
+    cv_fold_num = 5
+
+    learning_rate = 0.0005
+    threshold = 0.5
+
+    # model
+    col_num = 94  # XL
+    L_layer1_num = 48
+    L_layer2_num = 32
+    # L_layer1_num = 280
+    # L_layer2_num = 140
+
+    row_num = 656  # XR
+    R_layer1_num = 48
+    R_layer2_num = 32
+    # R_layer1_num = 280
+    # R_layer2_num = 140
+
+    keep_prob = 0.95
+    reg = 0.001
